ba4409859327af02945d9c848cced2ef0243db6b
Mustafa Rostom <mutafarostom@gmail.com>
2022-09-25 12:59:22 +0200

    add useRef hook

--- src/components/AddNewUser.jsx
+++ src/components/AddNewUser.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { useState,useEffect } from 'react'
+import { useState,useEffect, useRef } from 'react'
 
 const AddNewUser = (props) => {
     const{addUser , isEnabled} =props
     const [user,setUser]= useState({name:'',age:''})
+    const inpuEl=useRef(null); 
     const handleChange=(e)=>{
         const {name,value}=e.target
         setUser(currentUser=>({...currentUser,[name]:value }))
@@ -13,15 +14,20 @@ const AddNewUser = (props) => {
         addUser(user)
         setUser({name:"",age:''})
     }
+    // useEffect hook with array of adependancies 
     useEffect(()=>{
         if(!isEnabled){
             setUser({name:'',age:''})
         }
     },[isEnabled])
+    //simulate componentDidMount
+    useEffect(()=>{
+        inpuEl.current?.focus();        
+    },[])
 
     return (
     <form onSubmit={handleSubmit}>
-        <input type="text" name="name" value={user.name} onChange={handleChange}/>
+        <input type="text" name="name" value={user.name} onChange={handleChange} ref={inpuEl}/>
         <input type="number" name="age" value={user.age} onChange={handleChange  } />
         <input type="submit" value='Add User' disabled={!isEnabled} />
     </form>
