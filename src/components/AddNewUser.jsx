de30cf8362e2bb3eb558679dc1c68609d502111f
Mustafa Rostom <mutafarostom@gmail.com>
2022-09-27 18:43:17 +0200

    add useNavigate hook

--- src/components/AddNewUser.jsx
+++ src/components/AddNewUser.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import { useState,useEffect, useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const AddNewUser = (props) => {
     const{addUser , isEnabled} =props
     const [user,setUser]= useState({name:'',age:''})
     const inpuEl=useRef(null); 
+    const navigate=useNavigate( )
+
     const handleChange=(e)=>{
         const {name,value}=e.target
         setUser(currentUser=>({...currentUser,[name]:value }))
     }
+
     const handleSubmit=(event)=>{
         event.preventDefault();
         addUser(user)
         setUser({name:"",age:''})
+        navigate('/users')
     }
+
     // useEffect hook with array of adependancies 
     useEffect(()=>{
         if(!isEnabled){
             setUser({name:'',age:''})
         }
     },[isEnabled])
+
     //simulate componentDidMount
     useEffect(()=>{
         inpuEl.current?.focus();        
